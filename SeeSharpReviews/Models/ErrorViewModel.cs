add336d0004b766601692e6362fe83898405e50a
JoshuaSubray <subrayjoshua@gmail.com>
2026-02-25 19:28:09 -0500

    Initialized the project.

---
+++ SeeSharpReviews/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SeeSharpReviews.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
